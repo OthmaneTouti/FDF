99128582a9a508cd3ac16d3e31fa29243fa4f50b
OthmaneTouti <80723817+OthmaneTouti@users.noreply.github.com>
2024-01-08 17:32:11 -0500

    first commit

---
+++ fdf/src/fdf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ottouti <ottouti@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 21:58:03 by ottouti           #+#    #+#             */
+/*   Updated: 2024/01/08 14:24:39 by ottouti          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FDF_H
+# define FDF_H
+
+# include "../include/libft.h"
+# include <mlx.h>
+
+# include <math.h>
+
+typedef struct	s_mlx_data {
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_mlx_data;
+
+#endif
